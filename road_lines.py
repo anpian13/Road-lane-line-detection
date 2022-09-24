5c96ea248f24bbfcd6b3c3e5ba53c8d8c2a84db6
Eleventh <anpan1p2@gmail.com>
2022-09-24 14:52:32 +0300

    video with detected lines

--- road_lines.py
+++ road_lines.py
@@ -28,29 +28,48 @@ def draw_lines(image,lines):
 
 
 
-def process_image(image):
+def process_image(image,dash):
     # transform colors
-    # image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     
     # create vertices 
     imshape = image.shape
-    top_left = [imshape[1]/2-imshape[1]/4,imshape[0]/2+imshape[0]/9]
-    top_right = [imshape[1]/2+imshape[1]/4,imshape[0]/2+imshape[0]/9]
-    lower_left = [0,imshape[0]]
-    lower_right = [imshape[1],imshape[0]]
+    
+    # if case of dashcam region of interest changes:
+    # broaden top horizontal vertices points so all lanes visible
+    # set lower points higher so dashboard wouldn't be in region
+
+    
+    
+    # dash = False
+
+    if dash:
+        offset_top_horizontal = imshape[1]/4
+        offset_bottom_vert = imshape[0]/4
+
+    else:
+        offset_top_horizontal = 0
+        offset_bottom_vert = 0
+
+
+
+    top_left = [imshape[1]/2-imshape[1]/4 - offset_top_horizontal,imshape[0]/2+imshape[0]/9]
+    top_right = [imshape[1]/2+imshape[1]/4 + offset_top_horizontal,imshape[0]/2+imshape[0]/9]
+    lower_left = [0,imshape[0] - offset_bottom_vert]
+    lower_right = [imshape[1],imshape[0] - offset_bottom_vert]
     vertices = np.array([[lower_left,top_left,top_right,lower_right]],dtype=np.int32)
     
     # turn image gray for easier detection 
     image_gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY) 
 
     # use canny edge detection
-    image_canny = cv2.Canny(image_gray,100,200)
+
+    image_canny = cv2.Canny(image_gray,150,200)
 
     # crop image according to vertices
     cropped_image = region_of_interest(image_canny,vertices)
     
     # apply Hough transform to image
-    lines = cv2.HoughLinesP(cropped_image,rho=8,theta=np.pi/180,threshold=140,lines=np.array([]), minLineLength=40,maxLineGap=10)
+    lines = cv2.HoughLinesP(cropped_image,rho=6,theta=np.pi/180,threshold=140,lines=np.array([]), minLineLength=40,maxLineGap=10)
 
     # apply lines on image
     img_with_lines=draw_lines(image,lines)
@@ -58,12 +77,19 @@ def process_image(image):
 
 
 
+video_name = 'driving-through-Red-Rock-State-Park.mp4'
+# video_name = 'RAW-DASH-CAM.mp4'
 
-cap = cv2.VideoCapture('driving-through-Red-Rock-State-Park.mp4')
+# checking if video from dashcam based on file name
+if 'dash' in video_name.lower():
+    dash_flag = True
+else: dash_flag = False
+
+cap = cv2.VideoCapture(video_name)
 # cap = cv2.VideoCapture('Dashcam.mp4')
 while (cap.isOpened()):
     ret,frame =cap.read()
-    frame = process_image(frame)
+    frame = process_image(frame,dash_flag)
     cv2.imshow('frame',frame)
     if cv2.waitKey(1)&0xFF ==ord('q'):
         break
